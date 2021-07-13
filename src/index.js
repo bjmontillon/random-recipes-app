e5e2ee50f62085cc90ac12c434a75ddd01729011
bjmontillon <pangilinanbj@gmail.com>
2021-07-13 12:29:04 +0800

    new components added (header, reactplayer)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import ReactPlayer from 'react-player/youtube';
-
+import Responsiveplayer from './responsiveplayer/reactplayer'
+import Header from './header/header'
 import '../src/style.css';
+
+
 
 class App extends React.Component {
   constructor () {
@@ -53,80 +55,97 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-      <div className="wrapper">
-        <div className="section1" >
-          <h1>
-          {this.state.isFetchingRecipe
-            ? 'Loading Recipe...'
-            : this.state.strMeal}
-          </h1>
-          <img src={this.state.strThumb} alt='mealThumbnail' width="200" height="200" />
-          <br />
-          <a className="sourceLink" href={this.state.strSource} target="_blank">Source Link</a>
-          <br />
-          <button
-            className="myButton"
-            onClick={this.randomRecipe}
-            disabled={this.state.isFetchingRecipe}>Generate Recipe
-          </button>
-        </div>
+        <div className="main-container">
+          <div className="header">
+            <Header />
+          </div>
+          <div className="wrapper">
 
-        <div className="section2">
-          <div className='ingredients'>
-            <h1 className='hero_title'>Ingredients</h1>
-            <div className='ingredientsList'>
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient1}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient2}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient3}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient4}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient5}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient6}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient7}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient8}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient9}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient10}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient11}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient12}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient13}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient14}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient15}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient16}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient17}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient18}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient19}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient20}</p>))}
+            <div className="section1" >
+
+              <h1>
+                {this.state.isFetchingRecipe
+                  ? 'Loading Recipe...'
+                  : this.state.strMeal}
+              </h1>
+
+              <img src={this.state.strThumb} alt='mealThumbnail' width="200" height="200" />
+
+              <br />
+              <a className="sourceLink" href={this.state.strSource} target="_blank">Source Link</a>
+
+              <br />
+
+              <button
+                className="myButton"
+                onClick={this.randomRecipe}
+                disabled={this.state.isFetchingRecipe}>Generate Recipe
+              </button>
+            </div>
+
+            <div className="section2">
+              <div className='ingredients'>
+                <h1 className='hero_title'>Ingredients</h1>
+                <div className='ingredientsList'>
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient1}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient2}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient3}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient4}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient5}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient6}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient7}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient8}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient9}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient10}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient11}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient12}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient13}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient14}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient15}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient16}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient17}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient18}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient19}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strIngredient20}</p>))}
+                  </div>
+                <div className='measurementsList'>
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure1}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure2}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure3}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure4}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure5}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure6}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure7}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure8}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure9}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure10}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure11}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure12}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure13}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure14}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure15}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure16}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure17}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure18}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure19}</p>))}
+                    {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure20}</p>))}
+                  </div>
               </div>
-            <div className='measurementsList'>
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure1}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure2}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure3}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure4}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure5}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure6}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure7}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure8}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure9}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure10}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure11}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure12}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure13}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure14}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure15}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure16}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure17}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure18}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure19}</p>))}
-                {this.state.arraylist.map (item => (<p key={item.idMeal}>{item.strMeasure20}</p>))}
-              </div>
+            </div>
+              
+              
+          </div>
+          <div className="responsive-player">
+          <Responsiveplayer url={this.state.strYoutube} />
           </div>
         </div>
-      
-      </div>
-      <ReactPlayer url={this.state.strYoutube} />
       </div>
     );
   }
 }
+
+
+
 
 const rootElement = document.getElementById ('root');
 ReactDOM.render (<App />, rootElement);
---
+++ src/responsiveplayer/reactplayer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+import './responsive-player.css'
+
+const Responsiveplayer = ({ url }) => {
+    return (
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            url = { url }
+            width ='80%'
+            height ='80%'
+            controls = {true}
+          />
+        </div>
+      )
+}
+
+export default Responsiveplayer
